0c19657bb203b854f9f6e7ec3870882e8e24fbec
saiful7778 <saiful.islam.rafi.88@gmail.com>
2025-02-18 13:49:19 +0600

    Add topbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import "@/app/globals.css";
 import MainProvider from "@/providers/MainProvider";
+import Topbar from "@/components/shared/Topbar";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -22,10 +23,12 @@ export default function RootLayout({
     <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
       <body
         style={{ ...geistMono.style }}
-        className="antialiased w-full min-h-screen overflow-x-hidden py-4"
+        className="antialiased w-full min-h-screen overflow-x-hidden"
       >
         <MainProvider>
-          <header className="container mx-auto px-4"></header>
+          <header className="container mx-auto px-4 mb-4">
+            <Topbar />
+          </header>
           <main className="container mx-auto px-4">{children}</main>
         </MainProvider>
       </body>
---
+++ src/components/shared/Topbar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import ThemeToggle from "../shadcn/ThemeToggle";
+
+const Topbar: React.FC = () => {
+  return (
+    <div className="flex items-center gap-2 py-4 border-b">
+      <Link href="/" className="text-3xl uppercase font-bold">
+        LOGO
+      </Link>
+      <div className="ml-auto">
+        <ThemeToggle />
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
